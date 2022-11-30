60250c9abf933dd6a98a0dc469dd10ca36b75546
Ravil <karimra@mail.ru>
2022-11-30 23:06:52 +0300

    10task

---
+++ Homework/Exercise11/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ //и на выходе показывает вторую цифру этого числа.
+//
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int firstDigit = number / 100;
+int secondDigit = number  / 10 % 10;
+
+Console.WriteLine($"{secondDigit}");
